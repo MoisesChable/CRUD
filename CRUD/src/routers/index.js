304b8859e4f6bfe2e57a2caca95327da7b4ffcf2
MoisesTouchtastic <moises@touchtastic.com.mx>
2020-02-17 13:23:30 -0600

    se le añadio estilo al footer del admin

--- CRUD/src/routers/index.js
+++ CRUD/src/routers/index.js
@@ -12,26 +12,26 @@ router.get('/admin', async(req, res) => {
         datos: preguntas
     });
 });
-router.post('/admin/add', async(req, res) => {
+router.post('/add', async(req, res) => {
     //definicion de los datos
     const question = new Question(req.body);
     //almacenamiento en base de datos
     await question.save();
     res.redirect('/admin');
 });
-router.get('/admin/delete/:id', async(req, res) => {
+router.get('/delete/:id', async(req, res) => {
     const { id } = req.params;
     await Question.remove({ _id: id });
     res.redirect('/admin');
 });
 
-router.post('/admin/update/:id', async(req, res) => {
+router.post('/update/:id', async(req, res) => {
     const { id } = req.params;
     await Question.update({ _id: id }, req.body);
     res.redirect('/admin');
 });
 
-router.get('/admin/edit/:id', async(req, res) => {
+router.get('/edit/:id', async(req, res) => {
     const { id } = req.params;
     const datos = await Question.findById(id);
     res.render('edit', {
@@ -39,7 +39,7 @@ router.get('/admin/edit/:id', async(req, res) => {
     });
 });
 
-router.get('/admin/nuevo', async(req, res) => {
+router.get('/nuevo', async(req, res) => {
     res.render('nuevo');
 });
 
